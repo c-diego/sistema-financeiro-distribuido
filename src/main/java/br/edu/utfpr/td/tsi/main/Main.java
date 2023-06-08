0a6779f235f42f0a6ba0ddfaddc7bf1719880f4f
c-diego <diegocorte02@hotmail.com>
2023-06-08 19:38:12 -0300

    Creating project structure. Creating pom.xml. Adding dependencies to pom.xml. Creating docker-compose.yml.

---
+++ src/main/java/br/edu/utfpr/td/tsi/main/Main.java
@@ -0,0 +1,8 @@
+package br.edu.utfpr.td.tsi.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/br/edu/utfpr/td/tsi/MainTest.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.td.tsi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Testing project")
+public class MainTest {
+
+    @Test
+    @DisplayName("Test should pass")
+    public void test() {
+        assertTrue(true);
+    }
+
+}
