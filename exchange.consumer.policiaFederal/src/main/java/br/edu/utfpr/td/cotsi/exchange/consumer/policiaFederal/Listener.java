10d75da9efc4a8991261e181d3b076af847b0e38
andrelquiosi <andre.quiosi@Outlook.com>
2023-06-24 14:09:41 -0300

    Cricação da Classe Transação

--- exchange.consumer.policiaFederal/src/main/java/br/edu/utfpr/td/cotsi/exchange/consumer/policiaFederal/Listener.java
+++ exchange.consumer.policiaFederal/src/main/java/br/edu/utfpr/td/cotsi/exchange/consumer/policiaFederal/Listener.java
@@ -1,14 +1,25 @@
 package br.edu.utfpr.td.cotsi.exchange.consumer.policiaFederal;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
 
 @Component
 public class Listener {
+	@Autowired
+	Gson gson;
 
 	@RabbitListener(queues = "policia.federal")
 	public void listen(String in) {
-		System.out.println(in);
+		processarTransacao(in);
+	}
+
+	private void processarTransacao(String in) {
+
+		Transacao transacao = gson.fromJson(in, Transacao.class);
+		System.out.println(transacao);
+
 	}
 }
-
