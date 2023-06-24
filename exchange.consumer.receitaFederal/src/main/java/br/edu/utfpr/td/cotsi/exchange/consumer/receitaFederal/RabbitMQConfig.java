c9d53efee91ecf77d71d9fcc8e21767d2e90887a
andrelquiosi <andre.quiosi@Outlook.com>
2023-06-24 14:17:07 -0300

    Correção Configuration

--- exchange.consumer.receitaFederal/src/main/java/br/edu/utfpr/td/cotsi/exchange/consumer/receitaFederal/RabbitMQConfig.java
+++ exchange.consumer.receitaFederal/src/main/java/br/edu/utfpr/td/cotsi/exchange/consumer/receitaFederal/RabbitMQConfig.java
@@ -5,7 +5,9 @@ import javax.annotation.PostConstruct;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQConfig {
 
     @Autowired
